2544b04ccef736c13582ff9e293e33e3fdee81a1
LunaGod <wesminy@qq.com>
2021-03-30 10:20:57 +0800

    fix: vue type

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,13 +1,5 @@
-import { Viewer } from "cesium";
-
 declare module "*.vue" {
-  // @ts-ignore
   import { DefineComponent } from "vue";
   const component: DefineComponent<{}, {}, any>;
   export default component;
-}
-declare global {
-  interface Window {
-    map: Viewer;
-  }
 }
